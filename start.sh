c2a91ae5baeef415df1d797b70bd7ab06c37f452
rraisal <rraisal@gmail.com>
2022-07-08 15:50:44 +0530

    start.sh script completed

--- start.sh
+++ start.sh
@@ -18,6 +18,7 @@ sleep 2s;
 echo "[INFO]: Adding helm elastic repository at local";
 helm repo add elastic https://helm.elastic.co
 helm install elasticsearch elastic/elasticsearch --version 7.17.3 -f helm-charts/elasticsearch/values.yaml
+printf "\n"
 echo "[Success]: Elasticsearch Deployed"
 sleep 2s;
 echo "[INFO]: Enabling Addon Ingress for Minikube";
@@ -25,9 +26,20 @@ minikube addons enable ingress
 echo "[INFO]: Starting the Application using Helm";
 helm install app -f helm-charts/app/values.yaml ./helm-charts/app
 sleep 2s;
+printf "\n"
 echo "[Success]: App Deployed";
 printf "\n\n"
-echo "[Success]: Browse http://`minikube ip`"
+kubectl get pods
+printf "\n"
+echo "Minikube IP = `minikube ip`"
+printf "\n"
+echo "[INFO] Run following command to insert the first data into Elasticsearch once both the service Ready (This may take ~ 1 Minute)"
+printf "\n"
+echo "********************************************************************************************"
+echo "curl -X POST -F 'id=1' -F 'cityname=newyork' -F 'population=1000043' `minikube ip`/add_city"
+echo "********************************************************************************************"
 printf "\n\n"
+echo "[INFO]: Browse http://`minikube ip`/all_city after added the first data"
+printf "\n"
 }
 prepare
